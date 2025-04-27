0a15b710d0b7ff599d11f74bc3ec6934986162fd
Kipkerich <jkipkerich@kabarak.ac.ke>
2025-04-27 08:53:16 +0300

    login_redirect

--- mis/settings.py
+++ mis/settings.py
@@ -123,6 +123,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'login-url'
-LOGIN_REDIRECT_URL = 'home-url',
+LOGIN_REDIRECT_URL = 'home-url'
 LOGOUT_REDIRECT_URL = 'login-url'
 
